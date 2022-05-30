3032dde50d2f73b3dfe3c77d7334806d4ca4eb73
송민섭 <minsub1031@gmail.com>
2022-05-30 22:08:26 +0900

    config 변경

--- api/__init__.py
+++ api/__init__.py
@@ -4,14 +4,12 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
-import config
-
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(config)
+    app.config.from_envvar('APP_CONFIG_FILE')
     #ORM
     db.init_app(app)
     migrate.init_app(app, db)
---
+++ config/development.py
@@ -0,0 +1,7 @@
+from config.default import *
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'hy.db'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "dev"
+ERROR_404_HELP = False
+JWT_SECRET_KEY = "7x!QG[)'a?9psH?]"
