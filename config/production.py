a35815b31a512e6327e084ab4fbfc6085bef965c
송민섭 <minsub1031@gmail.com>
2022-05-31 00:12:02 +0900

    postgreSQL 연결

--- config/production.py
+++ config/production.py
@@ -1,5 +1,11 @@
 from config.default import *
+from dotenv import load_dotenv
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'hy.db'))
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'1\xf5\x13I:\xeb\xde\xf6\xdf^6\xd7\x00`\xe4\x7f'
